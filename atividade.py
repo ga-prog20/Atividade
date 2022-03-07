b3a8090e19968dfab33a1889b4e3b2d3aa289e25
Gabriel <gabrielcordeiro66@gmail.com>
2022-03-07 20:31:11 -0300

    Alterando Python

---
+++ atividade.py
@@ -0,0 +1,18 @@
+from string import printable
+
+
+nome ='Gabriel'
+
+curso='Analise e Desenvolvimento de Sistemas'
+
+nota1 = 7.5
+
+nota2 = 8.3
+
+media = (nota1 + nota2) / 2
+
+print('Nome do aluno', nome)
+print('Curso: ', curso)
+print('Nota1', nota1)
+print('Nota2', nota2)
+print('Media', media)
